4fc433dbb1575f8245ef3061a98c1b5e18096f64
avani <avanijain7891@gmail.com>
2020-02-27 16:43:49 +0530

    index code

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,26 +1,22 @@
 class PostsController < ApplicationController
 	def index
-     @posts = Post.where(status: 'publish').paginate(page: params[:page], per_page: 2)
+		if params[:search].blank?  
+			@posts = Post.where(status: 'publish').paginate(page: params[:page], per_page: 2)
+		else  
+			@parameter = params[:search].downcase 
+			category_ids = Category.where("name LIKE :search", search:  "%#{@parameter}%").ids
+			@posts = Post.where(category_id: category_ids).paginate(page: params[:page], per_page: 2)
+	  end 
 	end
-	
 	def show
 		@post     = Post.find(params[:id])
 		@comments = @post.comments
 	end
-
 	def showuser
 		#@users = User.all
 		@users = User.where.not(roles_mask: ['admin']) 
 	end
-
 	def search
-		if params[:search].blank?  
-			redirect_to(root_path, alert: "Empty field!")  
-		else  
-			@parameter = params[:search].downcase 
-      category_ids = Category.where("name LIKE :search", search:  "%#{@parameter}%").ids
-      @results = Post.where(category_id: category_ids) 
-		end  
 	end
 end
- #@posts = Post.where(category_id: params[:srearch].to_i) unless params[:search].blank?
+ 
