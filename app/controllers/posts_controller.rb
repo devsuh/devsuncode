5dfa4aa2fd2b43a02f97eb8012c775d839de439b
avani <avanijain7891@gmail.com>
2020-02-27 22:19:31 +0530

    code

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,13 +2,16 @@ class PostsController < ApplicationController
 	def index
 		if params[:search].blank?  
 			@posts = Post.where(status: 'publish').paginate(page: params[:page], per_page: 2)
-		else  
+		else 
 			@parameter = params[:search].downcase 
 			category_ids = Category.where("name LIKE :search", search:  "%#{@parameter}%").ids
-			@posts = Post.where(category_id: category_ids).paginate(page: params[:page], per_page: 2)
-	  end 
+			@posts = Post.where(category_id: category_ids).paginate(page: params[:page], per_page: 2)			
+		end 
+
+		
 	end
-	def show
+
+	def showuser
 		@post     = Post.find(params[:id])
 		@comments = @post.comments
 	end
@@ -17,6 +20,7 @@ class PostsController < ApplicationController
 		@users = User.where.not(roles_mask: ['admin']) 
 	end
 	def search
+
 	end
 end
  
